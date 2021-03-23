31e2b2a785d9c5ffde59b1b13f9f52901dc14c8d
achrefjarray <69697510+achrefjarray@users.noreply.github.com>
2021-03-23 21:50:51 +0100

    Update common.py

--- yolov5/models/common.py
+++ yolov5/models/common.py
@@ -35,7 +35,7 @@ class Conv(nn.Module):
         self.bn = nn.BatchNorm2d(c2)
         #self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
         #self.act =nn.GELU()
-        self.act =nn.Mish()
+        self.act =nn.Hardswish()
     def forward(self, x):
         return self.act(self.bn(self.conv(x)))
 
